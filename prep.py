9941ff44a611323d670acb638a29f8dbbb95c109
Your Name <you@example.com>
2018-06-26 19:53:20 -0500

    c

--- prep.py
+++ prep.py
@@ -2,12 +2,13 @@ from random import shuffle
 
 if __name__ == "__main__":
     ######################
-    filename = "ztest.csv"
-    n = 10
+    filename = "test.csv"
+    n = 270000
     ######################
     ds = open(filename).readlines()
     del ds[0]
     shuffle(ds)
-    pr = open("dstest.csv","w")
+    pr = open("dstest.csv","w+")
     for i in range(n):
-        pr.write(ds[i])
+    	pr.write(ds[i])
+    pr.close()
